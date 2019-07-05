633774d83f51d76ec4abac1e862d23997f0aa623
Michael McHolm <mmcholm@topechelon.com>
2019-07-05 10:24:46 -0400

    acpi.py fixed

--- bin/acpi.py
+++ bin/acpi.py
@@ -1,20 +1,23 @@
 #!/usr/bin/python3
 import subprocess
+def status(description):
+    if description == "Charging":
+        return "+"
+    elif description == "Discharging":
+        return "-"
+    else:
+        return "="
+
 output = subprocess.check_output(['acpi']).decode('ascii')
 outputSplit = output.split(",")
 percent = outputSplit[1].strip()
 chargeSplit = outputSplit[0].split(":")
-charge = chargeSplit[1].strip()
-isCharging = charge == "Charging"
-remainDesc = outputSplit[2].strip()
-qReturn = [percent]
-if "zero rate" in remainDesc:
-    qReturn.append("=")
-else:
+stat = status(chargeSplit[1].strip())
+qReturn = [percent, stat]
+if stat != "=":
+    remainDesc = outputSplit[2].strip()
     remainSplit = remainDesc.split(" ")
     remain = remainSplit[0]
-    sign = "+" if isCharging else "-"
-    qReturn.append(sign)
     qReturn.append(remain)
 
 print(" ".join(qReturn))
